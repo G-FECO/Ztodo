36cb19e3557bcf6741765762c78b8d16c9afca1e
mznx13579 <mznx13579@icloud.com>
2020-11-05 17:43:36 +0900

    [add] draggable 4 stage added

---
+++ front/src/layouts/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    Z-Todo List
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
